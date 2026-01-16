59a063ba67b5420e63cbb5be4ea91929b4c6d12a
22071034-TN <22071034@vnu.edu.vn>
2026-01-16 08:59:24 +0700

    Update logout.php

--- logout.php
+++ logout.php
@@ -1,17 +1,11 @@
 <?php
-require_once 'db.php'; // Để dùng biến $conn
-
-// Xóa Token trong Database (Để cookie cũ không còn tác dụng)
+require_once 'db.php';
 if (isset($_SESSION['user_id'])) {
     $uid = $_SESSION['user_id'];
     $conn->query("UPDATE users SET remember_token = NULL WHERE id = $uid");
 }
-
-// Xóa Session
 session_unset();
 session_destroy();
-
-// Xóa Cookie trình duyệt (Đặt thời gian về quá khứ)
 if (isset($_COOKIE['remember_user'])) {
     setcookie('remember_user', '', time() - 3600, "/");
 }
